d725fb360202052c02921cfdbe3e53ee8dcd7000
Ford <25stewartfo11@daretolearn.com>
2023-03-20 11:37:57 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# PyFlipper
+Flipper written in python for Hypixel Skyblock
+## Is it bannable?
+Idk I guess we'll see soon anough
+## How do I install it?
+Assuming you already have python and git and they're installed to path and that your using powershell
+```
+git clone https://github.com/redalpa333/pyflipper.git
+cd pyflipper
+python -m venv .venv
+.venv/Script/Activate.ps1
+pip install -r install.txt
+python main.py
+```
+or if it's already installed you can skip
+```
+python -m venv .venv
+```
+&
+```
+pip install -r install.txt
+```
+## How do I use it?
+F10 to paste the view auction command
+F9 to stop the program
---
+++ main.py
@@ -0,0 +1,65 @@
+import requests, nbt, io, base64, itertools, time
+from pynput import keyboard
+
+def main():
+    def on_release(key):
+            nonlocal inc
+            if key == keyboard.Key.f10:
+                kb.press('/')
+                kb.release('/')
+                time.sleep(0.1)
+                try:
+                    kb.type(f'viewauction {uuids[inc]}\n')
+                except IndexError:
+                    uuids = get_profitable()
+                    inc = 0
+                inc += 1
+            if key == keyboard.Key.f9:
+                # Stop listener
+                return False
+
+    def get_profitable():
+
+        items = []
+        
+        def get_attr_from_nbt(raw):
+            data = nbt.nbt.NBTFile(fileobj = io.BytesIO(base64.b64decode(raw)))
+            # returns TAG_Compound pretty much a dict with the keys 'id', 'attributes' (another TAG_Compund), timestamp, uuid)
+            extra_attr = data['i'][0]['tag']['ExtraAttributes']
+            return extra_attr
+        
+        number_pages = requests.get('https://api.hypixel.net/skyblock/auctions').json()['totalPages']
+        for page in range(number_pages): #temp just so shits faster :)
+            print(f'processing page {page}')
+            web_data = requests.get(f'https://api.hypixel.net/skyblock/auctions?page={page}')
+            #add a dict with the auction UUID as the key, price & item ID in a tuple to a list if it's BIN
+            items.append((str(get_attr_from_nbt(i['item_bytes'])['id']), i['uuid'], i['starting_bid']) for i in (j for j in web_data.json()['auctions'] if j['bin'] == True))
+        
+        print('Now processing combined data')
+        item_prices_dict = {}
+        for item_data in itertools.chain.from_iterable(items):
+            try:
+                item_prices_dict[item_data[0]][item_data[1]] = item_data[2]
+            except KeyError:
+                item_prices_dict[item_data[0]] = {item_data[1]:item_data[2]}
+        print('Now sorting')
+        for dict_item_key, dict_item_val in item_prices_dict.items():
+            item_prices_dict[dict_item_key] = dict(sorted(dict_item_val.items(), key = lambda val:val[1]))
+        uuids = []
+        for dict_item_key in item_prices_dict.keys():
+            prices = list(item_prices_dict[dict_item_key].values()) 
+            try:
+                if prices[1] / prices[0] - 1 > 0.1 and prices[1] - prices[0] >= 1000000:
+                    uuids.append(list(item_prices_dict[dict_item_key].keys())[0])
+            except IndexError:
+                pass
+        return uuids
+    
+    inc = 0
+    kb = keyboard.Controller()
+    uuids = get_profitable()
+    with keyboard.Listener(on_release=on_release) as listener:
+        listener.join()
+
+if __name__ == '__main__':
+    main()
